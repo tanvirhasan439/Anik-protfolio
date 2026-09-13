9c28094c0510369cda54737c87a580a579c88553
tanvirhasan439 <atanvirhasan6@gmail.com>
2026-09-14 00:46:13 +0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "anik-portfolio-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./1000067526.png",
+    "./20260906_123925~2.jpg",
+    "./Tanvir_Hasan_Anik_CV.pdf"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
